99d5dde510c3c7af7e42e4dfec8d9943107dba78
Natagel <114615044+Natagel@users.noreply.github.com>
2022-10-26 11:54:07 -0500

    Update main.cpp

--- simulacro/main.cpp
+++ simulacro/main.cpp
@@ -1,15 +1,4 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <iostream>
-
-[0:04 p. m., 24/10/2022] Ashley: #include<stdio.h>
-#include<stdlib.h>
 
 int main() {
 	float gallones = 1;
